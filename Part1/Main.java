a858c4b73a97a018da6c05ee37149304fbdd93bf
NafeesA35 <anafee2005@gmail.com>
2025-04-28 07:35:23 +0100

    Added functionalities to allow user to select racelength and race repetition.

--- Part1/Main.java
+++ Part1/Main.java
@@ -1,13 +1,12 @@
 package Part1;
-import Part2.GuiFrame;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        //int distance = inputInt("Enter the race distance between 10 and 25 :");
-        Race race = new Race(22);
+        int distance = inputInt("Enter the race distance between 10 and 25 :");
+        Race race = new Race(distance);
         Horse horse = new Horse('■', "Opio", 0.5);
         Horse horse2 = new Horse('█', "Roach", 0.5);
         Horse horse3 = new Horse('░', "Pasta", 0.5);
@@ -20,18 +19,27 @@ public class Main {
         race.addHorse(horse3, 3);
         race.addHorse(horse4, 4);
         race.addHorse(horse5, 5);
-        /* 
-        for (int i =0   ; i < 3; i++){
+
+        int raceTime = inputInt("How many times do you want the race to run? (1-5) : ");
+        if(raceTime < 1) {
+            System.out.println("Number less than 1 is not allowed. Defaulting to 1.");
+            raceTime = 1;
+        }else if(raceTime > 5) {
+            System.out.println("Number greater than 5 is not allowed. Setting to 5.");
+            raceTime = 5;
+        }
+
+        for (int i =0; i < raceTime; i++){
             race.startRace();
         }
  
 
-        */
 
 
+        /* 
         new GuiFrame(horses);
 
-
+        */
 
 
         /* 
@@ -51,10 +59,9 @@ public class Main {
         while (!valid) {
             try {
                 input = scanner.nextInt();
-                System.out.println("You entered: " + input);
                 valid = true;
             } catch (Exception e) {
-                System.out.println("You input MUST be an integer between 10 and 25: ");
+                System.out.println("You input MUST be an integer");
                 scanner.next(); 
             }
         }
